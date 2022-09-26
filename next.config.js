ac6a1d7c181d67103c4c67160fcb9102f8624136
habubey <habubey@outlook.com>
2022-09-26 17:34:59 +0300

    three commmit

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  basePath: process.env.NODE_ENV === "production" ? "/sakai-react" : "",
   publicRuntimeConfig: {
     // Will be available on both server and client
+
     contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
   },
 }
